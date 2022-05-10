393fa128021e27bb543eaafdae5567bdaea53068
toduko <teddyepich@gmail.com>
2022-05-10 20:11:32 +0300

    Added Optional generic class

---
+++ src/Optional.hpp
@@ -0,0 +1,82 @@
+#ifndef OPTIONAL_HPP_
+#define OPTIONAL_HPP_
+
+template <typename T>
+class Optional
+{
+private:
+  T *data;
+
+  void free()
+  {
+    delete this->data;
+    this->data = nullptr;
+  }
+
+  void setData(const T &data)
+  {
+    if (this->data != &data)
+    {
+      this->free();
+      this->data = new T(data);
+    }
+  }
+
+public:
+  Optional() : data(nullptr) {}
+
+  Optional(const T &data) : data(nullptr)
+  {
+    this->setData(data);
+  }
+
+  Optional(const Optional &other) : data(nullptr)
+  {
+    if (other.isNull())
+    {
+      this->free();
+    }
+    else
+    {
+      this->setData(other.getData());
+    }
+  }
+
+  Optional &operator=(const Optional &other)
+  {
+    if (this != &other)
+    {
+      if (other.isNull())
+      {
+        this->free();
+      }
+      else
+      {
+        this->setData(other.getData());
+      }
+    }
+
+    return *this;
+  }
+
+  ~Optional()
+  {
+    this->free();
+  }
+
+  bool isNull() const
+  {
+    return this->data == nullptr;
+  }
+
+  const T &getData() const
+  {
+    if (this->isNull())
+    {
+      throw "No data";
+    }
+    return *this->data;
+  }
+};
+
+#endif
